a9d1ff1109a593642cc0ca4a6e72bfc245971534
Felipesg-ADS <felipesg.ads@gmail.com>
2026-07-21 22:28:03 +0000

    incluindo a função scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,28 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado, codigo, nome_cidade;
+  int populaçao, pontos_tur;
+  float area, pib;
   // Área para entrada de dados
-    printf("CARTA 1");
-    
-  
-  // Área para exibição dos dados da cidade
-
+  // nesta parte é para exibir os dados que serão inseridos pelo usuário na carta
+  printf("Estado: ");
+  scanf("%s", &estado);
+  printf("Código: ");
+  scanf("%s", &codigo);
+  printf("Nome da cidade: ");
+  scanf("%s", &nome_cidade);
+  printf("População: ");
+  scanf("%d", &populaçao);
+  printf("Área: ");
+  scanf("%f", &area);
+  printf("PIB: ");
+  scanf("%f", &pib);
+  printf("Pontos turísticos: ");
+  scanf("%d", &pontos_tur);
+ 
+  // Área para exibição dos dados da idade
+  printf("CARTA 1\n");
 
 return 0;
 } 
