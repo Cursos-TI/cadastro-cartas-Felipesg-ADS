33c0c07d2abedb7e1a01ce2609d95fc096f36e94
Felipesg-ADS <felipesg.ads@gmail.com>
2026-07-22 19:42:57 +0000

    adicionei a carta 2 para o usuário preencher# Please enter the commit message for your changes. Lines starting

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,23 +11,39 @@ int main() {
   float area, pib;
   // Área para entrada de dados
   // nesta parte é para exibir os dados que serão inseridos pelo usuário na carta
-  printf("Estado: ");
-  scanf("%s", &estado);
-  printf("Código: ");
-  scanf("%s", &codigo);
-  printf("Nome da cidade: ");
-  scanf("%s", &nome_cidade);
-  printf("População: ");
-  scanf("%d", &populaçao);
-  printf("Área: ");
-  scanf("%f", &area);
-  printf("PIB: ");
-  scanf("%f", &pib);
-  printf("Pontos turísticos: ");
-  scanf("%d", &pontos_tur);
- 
-  // Área para exibição dos dados da idade
   printf("CARTA 1\n");
+  printf("Estado: ");
+    scanf("%s", &estado);
+  printf("Código: ");
+    scanf("%s", &codigo);
+  printf("Nome da cidade: ");
+    scanf("%s", &nome_cidade);
+  printf("População: ");
+    scanf("%d", &populaçao);
+  printf("Área: ");
+    scanf("%f", &area);
+  printf("PIB: ");
+    scanf("%f", &pib);
+  printf("Pontos turísticos: ");
+    scanf("%d", &pontos_tur); 
+  "\n";
+  printf("CARTA 2\n");
+  printf("Estado: ");
+    scanf("%s", &estado);
+  printf("Código: ");
+    scanf("%s", &codigo);
+  printf("Nome da cidade: ");
+    scanf("%s", &nome_cidade);
+  printf("População: ");
+    scanf("%d", &populaçao);
+  printf("Área: ");
+    scanf("%f", &area);
+  printf("PIB: ");
+    scanf("%f", &pib);
+  printf("Pontos turísticos: ");
+    scanf("%d", &pontos_tur);
+  
+  // Área para exibição dos dados da idade
 
 return 0;
 } 
